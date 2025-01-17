50d758f9f4cc5696afc697ed273f3c5d5cfa73eb
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-17 12:14:09 +0530

    correct it

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const port = 3000;
 app.use(express.json());
 dotenv.config();
+const authenticateJWT = require("./middlewares/authenticateJWT");
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
@@ -15,7 +16,29 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
   });
-
+let otpStore = {};
+//generate otp
+const generateOTP = () => {
+  const secret = onlostpointercapture.authenticator.generatSecret();
+  return onlostpointercapture.authenticator.generate(secret);
+};
+//send otp mobile using fast2sms
+const sendMessage = async (mobile, token) => {
+    const options = {
+      authorization: process.env.FAST2SMS_API_KEY,
+      message: `Your OTP verification code is ${token}`,
+      numbers: [mobile],
+    };
+  
+    try {
+      const response = await fast2sms.sendMessage(options);
+      return { success: true, message: "OTP sent successfully!" };
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+      return { success: false, message: "Failed to send OTP." };
+    }
+  };
+  
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
