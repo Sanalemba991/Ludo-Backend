d6c829bd5bf1fc0cb509da68017f71837859ab38
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-17 11:08:01 +0530

    MONGOOSE ADD

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
 app.use(express.json());
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
