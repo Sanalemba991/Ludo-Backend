20884cc8a7d7bedd5023a44a27b732ac0805ee6e
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-17 11:07:14 +0530

    app

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express =require("express")
-const dotenv = require("dotenv")
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
-
-
+app.use(express.json());
 
 app.listen(port, () => {
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
