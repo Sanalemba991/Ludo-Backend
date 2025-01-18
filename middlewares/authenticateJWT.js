9bd60d799c9ea79592ddd933988b25450debe412
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-18 12:31:57 +0530

    okay tare

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticateJWT = (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1];
+  const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    return res.status(403).json({ message: 'Unauthorized: No token provided' });
+    return res.status(403).json({ message: "Unauthorized: No token provided" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: 'Unauthorized: Invalid token' });
+      return res.status(403).json({ message: "Unauthorized: Invalid token" });
     }
     req.user = user;
     next();
