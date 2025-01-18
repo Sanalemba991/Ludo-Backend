27f71225d110f185319af03dd3af54150e0ea76e
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-18 12:30:54 +0530

    okay

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -11,7 +11,7 @@ const authenticateJWT = (req, res, next) => {
     if (err) {
       return res.status(403).json({ message: 'Unauthorized: Invalid token' });
     }
-    req.user = user; // Store user info in req.user
+    req.user = user;
     next();
   });
 };
